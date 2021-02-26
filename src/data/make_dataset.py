447f55c9e2dafce0b662c1aabed534f947276bfa
Darren Liu <dliu9999@gmail.com>
2021-02-25 16:55:12 -0800

    more notebooks

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -35,11 +35,11 @@ def user_query(user, since, until, pandas=True, csv=False, output='test.csv'):
     Tweets_df = twint.storage.panda.Tweets_df
     return Tweets_df
 
-# dw = tweets_query('#bts', '2014-08-17', '2014-09-02', csv=True, output='../../data/raw/dark_wild.csv')
+# dw = tweets_query('#btsdarkandwild', '2014-08-17', '2014-09-02', csv=True, output='../../data/raw/dark_wild1.csv')
 # wings = tweets_query('#bts', '2016-10-08', '2016-10-24', csv=True, output='../../data/raw/wings.csv')
-# love = tweets_query('#bts', '2018-05-16', '2018-06-1', csv=True, output='../../data/raw/love.csv')
+# love = tweets_query('#love_yourself', '2018-05-16', '2018-06-1', csv=True, output='../../data/raw/love1.csv')
 # map_ = tweets_query('#bts', '2020-02-19', '2020-03-04', csv=True, output='../../data/raw/map.csv')
-# be = tweets_query('#bts', '2020-11-18', '2020-12-03', csv=True, output='../../data/raw/be.csv')
+# be = tweets_query('#bts_be', '2020-11-18', '2020-12-04', csv=True, output='../../data/raw/bts/be1.csv')
 
 # nine = tweets_query('#ts1989', '2014-10-25', '2014-11-10', csv=True, output='../../data/raw/taylor/nine1.csv')
 # reputation = tweets_query('#reputation', '2017-11-08', '2017-11-24', csv=True, output='../../data/raw/taylor/reputation1.csv')
@@ -69,3 +69,9 @@ def user_query(user, since, until, pandas=True, csv=False, output='test.csv'):
 # justin1 = user_query('27260086', '2012-06-13', '2012-06-29', csv=True, output='../../data/raw/justin/justin1.csv')
 # justin2 = user_query('27260086', '2015-11-11', '2015-11-27', csv=True, output='../../data/raw/justin/justin2.csv')
 # justin3 = user_query('27260086', '2020-02-12', '2020-02-28', csv=True, output='../../data/raw/justin/justin3.csv')
+
+# be = tweets_query('#bts', '2020-11-30', '2020-12-04', csv=True, output='../../data/raw/be1.csv')
+
+dw = tweets_query('#btsdarkandwild', '2014-08-17', '2014-09-02', csv=True, output='../../data/raw/bts/dark_wild1.csv')
+love = tweets_query('#love_yourself', '2018-05-16', '2018-06-1', csv=True, output='../../data/raw/bts/love1.csv')
+be = tweets_query('#bts_be', '2020-11-18', '2020-12-04', csv=True, output='../../data/raw/bts/be1.csv')
